83ec536e833bb76435f86e560f28c0a53c766408
Emma baby bomvll@naver.com <Emma baby bomvll@naver.com>
2021-01-23 21:34:04 +0900

    Except .gitignore Add Jpa setting

---
+++ src/main/java/com/emma/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.emma.book.springboot.domain.posts;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
